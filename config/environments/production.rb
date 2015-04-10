dd4c78f5c6a10e984b8327e3210611fe23cebf63
Stephen La <lastephen0@gmail.com>
2015-04-10 13:31:44 -0700

    fix environment files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,13 +83,13 @@ Rails.application.configure do
   config.log_level = :debug
 
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_host_name => 's3-us-west-2.amazonaws.com',
-  :s3_credentials => {
-    :bucket => "cloudsound",
-    :access_key_id => ENV['AWS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET']
+    :storage => :s3,
+    :s3_host_name => 's3-us-west-2.amazonaws.com',
+    :s3_credentials => {
+      :bucket => "cloudsound",
+      :access_key_id => ENV['AWS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET']
+    }
   }
-}
 
 end
