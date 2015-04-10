6e5288c7d8ccb23b999b00e2dd9536cc19da4bab
Stephen La <lastephen0@gmail.com>
2015-04-10 09:31:46 -0700

    Change current user ivar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return nil if self.session[:session_token].nil?
-    @user ||= User.find_by(session_token: self.session[:session_token])
+    @current_user ||= User.find_by(session_token: self.session[:session_token])
   end
 
   def log_in!(user)
