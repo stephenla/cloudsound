8d7faa16f4be1ada0a940f753dab9ffa81bb79d8
Stephen La <lastephen0@gmail.com>
2015-04-14 23:29:58 -0700

    styling on comment new section

--- app/assets/javascripts/views/comments/comment_new.js
+++ app/assets/javascripts/views/comments/comment_new.js
@@ -1,6 +1,6 @@
 Cloudsound.Views.CommentNew = Backbone.View.extend({
   template: JST['comments/comment_new'],
-
+  className: "comment-form-wrapper",
   render: function () {
     var content = this.template({ track: this.model });
     this.$el.html(content);
