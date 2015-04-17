69490e28b4d059e2396235562774acb546a3da20
Stephen La <lastephen0@gmail.com>
2015-04-17 14:51:19 -0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# 
+#
 # 2.times do
 #   Track.create! audio: File.new("/Users/stephen/Downloads/Hey Brother (TEEMID   Tessa Rose Jackson Cover).mp3", "r"), title: "brother", user_id: 1,
 #   avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})",
@@ -21,18 +21,18 @@
 #   track_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
 # end
 
-# 100.times do |num|
+# 200.times do |num|
 #   Comment.create! user_id: rand(99) + 20, track_id: rand(7) + 273, content: Faker::Hacker.say_something_smart
 # end
-# guest = User.where(username: "frank_farrell")
-# stephen = User.first
-# 50.times do |n|
-#   user = User.create!(username: Faker::Name.name, password: "password")
-#   guest.first.follow(user)
-#   stephen.follow(user)
-# end
-# 50.times do |n|
-#   user = User.create!(username: Faker::Name.name, password: "password")
-#   user.follow(guest.first)
-#   user.follow(stephen)
-# end
+guest = User.where(username: "frank_farrell")
+stephen = User.first
+50.times do |n|
+  user = User.create!(username: Faker::Name.name, password: "password")
+  guest.first.follow(user)
+  stephen.follow(user)
+end
+50.times do |n|
+  user = User.create!(username: Faker::Name.name, password: "password")
+  user.follow(guest.first)
+  user.follow(stephen)
+end
