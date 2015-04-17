2067b73a9ec108463071017b07d13b947123d628
Stephen La <lastephen0@gmail.com>
2015-04-17 09:17:26 -0700

    fix migration

--- db/migrate/20150416184108_add_track_gradient_to_track.rb
+++
@@ -1,5 +0,0 @@
-class AddTrackGradientToTrack < ActiveRecord::Migration
-  def change
-      add_column :tracks, :track_gradient, :string, null: false, default: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
-  end
-end
