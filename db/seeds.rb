07c73e6fdfc5968ca05941730054c99214f06203
Stephen La <lastephen0@gmail.com>
2015-04-20 18:04:45 -0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,53 +7,53 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 
-## Users ##
-# User.destroy_all
-# Track.destroy_all
-# stephen = User.create!(username: "cloudsound", password: "password",
-# avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
-# )
-#
-# guest = User.create!(username: "the_visitor", password: "password",
-# avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
-# )
-#
-# 50.times do |n|
-#   user = User.create!(username: Faker::Name.name, password: "password", avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})")
-#   guest.follow(user)
-#   stephen.follow(user)
-# end
-# 50.times do |n|
-#   user = User.create!(username: Faker::Name.name, password: "password", avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})")
-#   user.follow(guest)
-#   user.follow(stephen)
-# end
+# Users ##
+
+stephen = User.create!(username: "cloudsound", password: "password",
+avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
+)
+
+guest = User.create!(username: "the_visitor", password: "password",
+avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
+)
+guest.follow(stephen)
+stephen.follow(guest)
+50.times do |n|
+  user = User.create!(username: Faker::Name.name, password: "password", avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})")
+  guest.follow(user)
+  stephen.follow(user)
+end
+50.times do |n|
+  user = User.create!(username: Faker::Name.name, password: "password", avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})")
+  user.follow(guest)
+  user.follow(stephen)
+end
 
 
 ## Tracks ##
-1.times do
-  Track.create! audio: File.new("/Users/stephen/Downloads/cloudsound/Hey Brother (TEEMID   Tessa Rose Jackson Cover).mp3", "r"),
-  title: "brother",
-  user_id: 1,
-  avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})",
-  track_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
-  Track.create! audio: File.new("/Users/stephen/Downloads/cloudsound/i carmacked bastille.mp3", "r"),
-  title: "carmacked",
-  user_id: 1,
-  avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})",
-  track_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
-  Track.create! audio: File.new("/Users/stephen/Downloads/cloudsound/Something About Us (Studio Version).mp3", "r"),
-  title: "something", user_id: 1,
-  avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})",
-  track_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
-  Track.create! audio: File.new("/Users/stephen/Downloads/cloudsound/Gulch.mp3", "r"),
-  title: "gulch", user_id: 1,
-  avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})",
-  track_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
-end
+# 1.times do
+#   Track.create! audio: File.new("/Users/stephen/Downloads/cloudsound/Hey Brother (TEEMID   Tessa Rose Jackson Cover).mp3", "r"),
+#   title: "brother",
+#   user_id: 1,
+#   avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})",
+#   track_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
+#   Track.create! audio: File.new("/Users/stephen/Downloads/cloudsound/i carmacked bastille.mp3", "r"),
+#   title: "carmacked",
+#   user_id: 1,
+#   avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})",
+#   track_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
+#   Track.create! audio: File.new("/Users/stephen/Downloads/cloudsound/Something About Us (Studio Version).mp3", "r"),
+#   title: "something", user_id: 1,
+#   avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})",
+#   track_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
+#   Track.create! audio: File.new("/Users/stephen/Downloads/cloudsound/Gulch.mp3", "r"),
+#   title: "gulch", user_id: 1,
+#   avatar_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})",
+#   track_gradient: "linear-gradient(#{rand(0..360)}deg, ##{"%06x" % (rand * 0xffffff)}, ##{"%06x" % (rand * 0xffffff)})"
+# end
 
-#
-# ## Comments ##
+
+## Comments ##
 # 150.times do |num|
 #   track = Track.all.sample
 #   user_id = User.all.sample.id
